3485ecaa2a9cb84c6e31c152a1462c6575d36ff4
Asuka-Aozora <azdin1411@gmail.com>
2025-09-22 12:30:14 +0700

    tracking and parsing data to database

---
+++ blog-be/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config"
+
+const server = express()
+let PORT = 3000;
+
+server.use(express.json())
+
+mongoose.connect(process.env.MONGO_URI, {
+    autoIndex: true
+});
+
+server.post("/signup", (req, res) => {
+    
+    let { fullname, email, password } = req.body;
+
+    // validating the data from frontend 
+    if (fullname.length < 3) {
+        return res.status(403).json({ "error": "Fullname must be at least 3 letters long" })
+    }
+
+    return res.status(200).json( {"status": "okay"} )
+})
+
+server.listen(PORT, () => {
+    console.log(`listening on port -> ${PORT}`);
+    
+})
